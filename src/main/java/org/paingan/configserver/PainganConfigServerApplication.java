0594d038e91c48419f993cde8d2cd3aff88d0e2f
paulusyansen <newcurzon@yahoo.com>
2018-09-17 13:27:06 +0700

    enable config server

--- src/main/java/org/paingan/configserver/PainganConfigServerApplication.java
+++ src/main/java/org/paingan/configserver/PainganConfigServerApplication.java
@@ -2,8 +2,10 @@ package org.paingan.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class PainganConfigServerApplication {
 
 	public static void main(String[] args) {
